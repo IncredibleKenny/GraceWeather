3013bf8c379b29470bfb810b159a7dcb3be302c3
Kenny <kennychan291@qq.com>
2017-03-31 10:03:34 +0800

    bug fix- define a global weatherId in weather.activity

--- app/src/main/java/com/kenny/graceweather/WeatherActivity.java
+++ app/src/main/java/com/kenny/graceweather/WeatherActivity.java
@@ -49,6 +49,7 @@ public class WeatherActivity extends AppCompatActivity {
     public SwipeRefreshLayout swipeRefreshLayout;
     public DrawerLayout drawerLayout;
     private Button navButton;
+    private String mWeatherId;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -87,22 +88,22 @@ public class WeatherActivity extends AppCompatActivity {
         }else{
             loadBingPic();
         }
-        final String weatherId;
+       // final String weatherId;  //wrong
         String weatherString = prefs.getString("weather",null);
         if (weatherString != null) {
             Weather weather = Utility.handleWeatherResponse(weatherString);
-            weatherId = weather.basic.weatherId; //TODO final一次赋值 永不改变
+            mWeatherId = weather.basic.weatherId; //TODO final一次赋值 永不改变
             showWeatherInfo(weather);
         }else{
-            weatherId = getIntent().getStringExtra("weather_id");
+            mWeatherId= getIntent().getStringExtra("weather_id");
             weatherLayout.setVisibility(View.INVISIBLE);
-            requestWeather(weatherId);
+            requestWeather(mWeatherId);
         }
         swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
             @Override
             public void onRefresh() {
 
-                requestWeather(weatherId);
+                requestWeather(mWeatherId);
             }
         });
         navButton.setOnClickListener(new View.OnClickListener() {
@@ -167,6 +168,7 @@ public class WeatherActivity extends AppCompatActivity {
                             SharedPreferences.Editor editor = PreferenceManager.getDefaultSharedPreferences(WeatherActivity.this).edit();
                             editor.putString("weather",responseText);
                             editor.apply();
+                            mWeatherId = weather.basic.weatherId;
                             showWeatherInfo(weather);
                         }else{
                             Toast.makeText(WeatherActivity.this, "获取天气信息失败", Toast.LENGTH_SHORT).show();
